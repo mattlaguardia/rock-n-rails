6caf04cee3d689a65581beddb11d6167b1c2e62a
mattlaguardia <matthewlaguardia@gmail.com>
2016-06-14 15:22:28 -0700

    adds seed file

--- rock-n-rails/db/seeds.rb
+++ rock-n-rails/db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Record.destroy_all
+
+Record.create([
+  {
+    title: "On Avery Island",
+    artist: "Neutral Milk Hotel",
+    year: 1996,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/7/73/On_avery_island_album_cover.jpg",
+    song_count: 12
+  },
+  {
+    title: "Everything All the Time",
+    artist: "Band of Horses",
+    year: 2006,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/5/51/BandofHorsesEverythingalltheTime.jpg",
+    song_count: 10
+  },
+  {
+    title: "The Flying Club Cup",
+    artist: "Beirut",
+    year: 2007,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/4/4c/The_Flying_Club_Cup.jpg",
+    song_count: 13
+  },
+  {
+    title: "The Blue Album",
+    artist: "Weezer",
+    year: 1994,
+    cover_art: "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Weezer_-_Blue_Album.png/220px-Weezer_-_Blue_Album.png",
+    song_count: 10
+  }
+])
